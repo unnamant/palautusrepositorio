fbfce368ca1ac046c9b7c02a1396d9b85a9e6b04
Unna Mäntyniemi <unna.mantyniemi@helsinki.fi>
2025-11-02 14:51:55 +0200

    Tehty tehtävä 14, lisätty sovellukseen uusi testi

---
+++ viikko1/riippuvuuksien-injektointi/src/tests/laskin_test.py
@@ -0,0 +1,32 @@
+import unittest
+from laskin import Laskin
+
+
+class StubIO:
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.outputs = []
+
+    def lue(self, teksti):
+        return self.inputs.pop(0)
+
+    def kirjoita(self, teksti):
+        self.outputs.append(teksti)
+
+
+class TestLaskin(unittest.TestCase):
+    def test_yksi_summa_oikein(self):
+        io = StubIO(["1", "3", "-9999"])
+        laskin = Laskin(io)
+        laskin.suorita()
+
+        self.assertEqual(io.outputs[0], "Summa: 4")
+
+    def test_kaksi_summaa_oikein(self):
+        io = StubIO(["1", "2", "4", "3", "-9999"])
+        laskin = Laskin(io)
+        laskin.suorita()
+
+        self.assertEqual(io.outputs[0], "Summa: 3")
+        self.assertEqual(io.outputs[1], "Summa: 7")
+                         
