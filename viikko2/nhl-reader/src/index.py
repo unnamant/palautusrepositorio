e984bd719b30fd8bc6d4eccd5dae29526fbf26ee
Unna Mäntyniemi <unna.mantyniemi@helsinki.fi>
2025-11-09 13:23:42 +0200

    Graphic Player list

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,30 @@
-from player import Player
+from player import PlayerReader, PlayerStats
+from rich.console import Console
+from rich.table import Table
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+
+    season = input("Season: ")
+    nationality = input("Nationality: ")
+
+    players = stats.top_scorers_by_nationality(nationality)
+    
+    console = Console()
+
+    table = Table(title=f"{season} players from {nationality}")
+    table.add_column("Released", style="cyan")
+    table.add_column("Team", style="magenta")
+    table.add_column("Goals", justify="right", style="yellow")
+    table.add_column("Assists", justify="right", style="yellow")
+    table.add_column("Points", justify="right", style="yellow")
+
+    for player in players:
+        table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.goals + player.assists))
+
+    console.print(table)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,42 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.team = dict['team']
+        self.goals = dict['goals'] 
+        self.nationality = dict['nationality']
+        self.assists = dict['assists']
+
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.team} {self.goals} + {self.assists} = {self.goals + self.assists}"
+
+class PlayerReader:
+    def __init__(self, url: str):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+
+        players = []
+        for player_dict in response: 
+            player = Player(player_dict) 
+            players.append(player)
+
+        return players
+
+class PlayerStats:
+    def __init__(self, reader: PlayerReader):
+        self.reader = reader
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality: str):
+        players = []
+        for player in self.players:
+            if player.nationality == nationality:
+                players.append(player)
+        sorted_players = sorted(players, key=lambda player: player.goals + player.assists, reverse=True)
+        return sorted_players
+
+    def __str__(self):
+        return (player for player in self.players)
