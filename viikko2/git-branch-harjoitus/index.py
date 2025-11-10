07ee27f151ee5a20dc376b405a854c105101230d
Unna Mäntyniemi <unna.mantyniemi@helsinki.fi>
2025-11-10 10:36:21 +0200

    Change index.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,9 +1,13 @@
 from logger import logger
+from summa import summa
+from erotus import erotus
 
 logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
+print(f"{summa(x, y)}")
+print(f"{erotus(x, y)}")
 
 logger("lopetetaan")
 
